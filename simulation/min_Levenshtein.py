b4297926500478807a34651adb787fb5a341b804
nmateyko <nick.mateyko@gmail.com>
2022-11-03 14:48:50 -0700

    Create min_Levenshtein script

---
+++ simulation/min_Levenshtein.py
@@ -0,0 +1,22 @@
+import random
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from Levenshtein import distance
+
+LENGTH = 30
+SIZE = 100000
+
+random_seqs = ["".join(random.choices(['A', 'C', 'G', 'T'], k=LENGTH)) for i in range(SIZE)]
+
+min_dists = []
+for i, seq1 in tqdm(enumerate(random_seqs)):
+  min = LENGTH
+  for j, seq2 in enumerate(random_seqs):
+    if i != j:
+      d = distance(seq1, seq2)
+      if d < min:
+        min = d
+  min_dists.append(d)
+
+plt.hist(min_dists, bins=(range(max(min_dists) + 1)))
+plt.show()
