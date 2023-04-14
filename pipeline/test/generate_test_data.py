96b183da88809195947d5e2b9abf7a423d4c1f4c
Nicholas Mateyko <nick.mateyko@gmail.com>
2023-04-14 09:05:10 -0700

    Add test seqs

--- pipeline/test/generate_test_data.py
+++ pipeline/test/generate_test_data.py
@@ -1,28 +1,40 @@
-# This script generates two test fastq.gz files (read 1 and read 2) to run through the PLAB pipeline
+# This script generates four test fastq.gz files (read 1 and read 2 for input and assembled)
+# to run through the GibsPred pipeline. The read structure matches the PCR-adaptered samples,
+# not the ligation-adaptered samples.
+
+# Input should have 5 A30, 8 T30, 6 G30, and 4 C30 sequences.
+# Assembled should have 5 A30, 5 T30, 12 G30, and 4 C30 sequences.
 
 import gzip
 import random
 
-with open('test_seqs.txt', 'r') as f:
-    seqs = [line.strip() for line in f]
+with open('test_seqs_input.txt', 'r') as f:
+    seqs_input = [line.strip() for line in f]
+
+with open('test_seqs_assembled.txt', 'r') as f:
+    seqs_assembled = [line.strip() for line in f]
 
 def rev_comp(seq):
     comp = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
     return ''.join([comp[base] for base in seq[::-1]])
 
 # shuffle seqs
-random.shuffle(seqs)
+random.shuffle(seqs_input)
+random.shuffle(seqs_assembled)
 
-seqs_revcomp = [rev_comp(seq) for seq in seqs]
+revcomp_input = [rev_comp(seq) for seq in seqs_input]
+revcomp_assembled = [rev_comp(seq) for seq in seqs_assembled]
 
-# add r1 upstream and downstream to seqs and r2 upstream and downstream to seqs_revcomp
-r1_upstream = "TGCATTTTTTTCACATC"
-r1_downstream = "GGTTACGGCTGTT"
-r2_upstream = "AACAGCCGTAACC"
-r2_downstream = "GATGTGAAAAAAATGCA"
+# add r1 upstream and downstream to seqs and r2 upstream and downstream to revcomp
+r1_upstream = "CGCCAGCTCTTC"
+r1_downstream = "GAAGAGCCACTGGCCGTCGTTTTACAACGTCGTGACTGGGAAAACCCTGGCGTTACCCAACTTAATCGCCTTGCAGCACATAGATCGGAAGAGCACACGTCTGAACTCCAGTCAC"
+r2_upstream = "ATGTGCTGCAAGGCGATTAAGTTGGGTAACGCCAGGGTTTTCCCAGTCACGACGTTGTAAAACGACGGCCAGTGGCTCTTC"
+r2_downstream = "GAAGAGCTGGCGAGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT"
 
-seqs = [r1_upstream + seq + r1_downstream for seq in seqs]
-seqs_revcomp = [r2_upstream + seq + r2_downstream for seq in seqs_revcomp]
+seqs_input = [r1_upstream + seq + r1_downstream for seq in seqs_input]
+revcomp_input = [r2_upstream + seq + r2_downstream for seq in revcomp_input]
+seqs_assembled = [r1_upstream + seq + r1_downstream for seq in seqs_assembled]
+revcomp_assembled = [r2_upstream + seq + r2_downstream for seq in revcomp_assembled]
 
 def generate_fastq(seqs, filename, read):
     with gzip.open(filename, 'wt') as f:
@@ -32,5 +44,7 @@ def generate_fastq(seqs, filename, read):
             f.write(f"+\n")
             f.write(f"{'F' * len(seq)}\n")
 
-generate_fastq(seqs, 'test_r1.fastq.gz', '1')
-generate_fastq(seqs_revcomp, 'test_r2.fastq.gz', '2')
+generate_fastq(seqs_input, 'test_input_r1.fastq.gz', '1')
+generate_fastq(revcomp_input, 'test_input_r2.fastq.gz', '2')
+generate_fastq(seqs_assembled, 'test_assembled_r1.fastq.gz', '1')
+generate_fastq(revcomp_assembled, 'test_assembled_r2.fastq.gz', '2')
